8aa6808c021f5b60084b9c4a066e1dda7d00a7b4
xbz <jlxbz@sina.com>
2018-09-24 16:41:20 +0800

    main structor

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,103 @@
 <template>
-  <div id="app">
+  <div id="app" :style="{backgroundImage: `url(${require('~/bgc_guangfu.png')})`}">
     <Header/>
-    <div class='body' style="background: ${require('~/pane-bottom.png')}">
+    <div class='body' :style="{backgroundImage: `url(${require('~/pane-bottom.png')})`}">
+      <NavTab/>
+      <!--ul class='leftmenu'>
+        <li>
+          <router-link to='/AlarmMonitor'>报警参数监控</router-link>
+        </li>
+        <li>
+          <router-link to='/AlarmMap'>报警地图</router-link>
+        </li>
+        <li>
+          <router-link to='/AlarmStatis'>报警统计分析</router-link>
+        </li>
+        <li>
+          <router-link to='/AlarmCategoryStatis'>报警分类统计</router-link>
+        </li>
+      </ul>
+      <ul-- class='rightmenu'>
+        <li>
+          <router-link to='/AlarmCapability'>报警性能评估</router-link>
+        </li>
+        <li>
+          <router-link to='/SysAlarmJudge'>分系统报警性能评估</router-link>
+        </li>
+        <li>
+          <router-link to='/KeyPointerMonitor'>关键报警点监测</router-link>
+        </li>
+        <li>
+          <router-link to='/AlarmChangeManager'>报警变革管理</router-link>
+        </li>
+      </ul-->
       <router-view/>
     </div>
   </div>
 </template>
 
 <script>
-import Header from '@/components/basic/Header.vue'
+import Header from '@/components/Header.vue'
+import NavTab from '@/components/NavTab.vue'
 export default {
   name: 'App',
+  data(){
+    return {
+
+    }
+  },
   components: {
     Header,
+    NavTab,
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: 100% 100%;
+
+  div.body{
+    position: absolute;
+    width: 100%;
+    height: calc(100% - 55px);
+    background-position: center;
+    background-size: cover;
+    ul{
+      list-style-type: none;
+      display: flex;
+      float: left;
+      li{
+        margin-right: 13px;
+      }
+      position: absolute;
+      top: -35px;
+      a{
+        color: #ffff;
+        font-size: 15px;
+        text-decoration: none;
+      }
+
+    }
+    ul.leftmenu {
+      left: 10px;
+      li{
+        margin-right: 25px;
+      }
+    }
+    ul.rightmenu{
+      right: 30px;
+    }
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AlarmMonitor from '@/components/business/AlarmMonitor'
+import AlarmCapability from '@/components/business/AlarmCapability'
+import AlarmCategoryStatis from '@/components/business/AlarmCategoryStatis'
+import AlarmChangeManager from '@/components/business/AlarmChangeManager'
+import AlarmMap from '@/components/business/AlarmMap'
+import AlarmStatis from '@/components/business/AlarmStatis'
+import KeyPointerMonitor from '@/components/business/KeyPointerMonitor'
+import SysAlarmJudge from '@/components/business/SysAlarmJudge'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: '',
-      component:null
+      path: '/AlarmMonitor',
+      name: 'AlarmMonitor',
+      component: AlarmMonitor
+    },
+    {
+      path: '/AlarmCapability',
+      name: 'AlarmCapability',
+      component: AlarmCapability
+    },
+    {
+      path: '/AlarmCategoryStatis',
+      name: 'AlarmCategoryStatis',
+      component: AlarmCategoryStatis
+    },
+    {
+      path: '/AlarmStatis',
+      name: 'AlarmStatis',
+      component: AlarmStatis
+    },
+    {
+      path: '/AlarmChangeManager',
+      name: 'AlarmChangeManager',
+      component: AlarmChangeManager
+    },
+    {
+      path: '/AlarmMap',
+      name: 'AlarmMap',
+      component: AlarmMap
+    },
+    {
+      path: '/KeyPointerMonitor',
+      name: 'KeyPointerMonitor',
+      component: KeyPointerMonitor
+    },
+    {
+      path: '/SysAlarmJudge',
+      name: 'SysAlarmJudge',
+      component: SysAlarmJudge
     }
   ]
 })
